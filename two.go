659767162d77816f627e327152a0975c052d4691
Brian Flad <bflad417@gmail.com>
2021-05-06 09:26:58 -0400

    Add call to Three()

--- two.go
+++ two.go
@@ -2,8 +2,9 @@ package two
 
 import (
 	one "github.com/bflad/go-module-one"
+	three "github.com/bflad/go-module-three"
 )
 
 func Two() string {
-	return "two is greater than " + one.One()
+	return "two is greater than " + one.One() + " and less than " + three.Three()
 }
